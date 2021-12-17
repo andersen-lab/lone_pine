61f6fe6c3fb320f361935e0385d9895bdbfdf2da
nate <snowboardman007@gmail.com>
2021-12-17 11:33:27 -0800

    updated summary text

--- src/sgtfpage.py
+++ src/sgtfpage.py
@@ -4,7 +4,7 @@ import src.plot as dashplot
 
 def get_layout( sgtf_data ):
     markdown = """
-    To gain insights in the spread of the Omicron variant in out community, we are working with a large number of 
+    To gain insight into the spread of the Omicron variant in our community, we are working with a large number of 
     partners to track S-gene target failures (SGTFs). SGTFs are a feature of the TaqPath PCR assay that fails to detect
     the spike gene of certain variants of interest due to a deletion in these viruses' spike gene. Most Omicron 
     sequences have this deletion while most Delta sequences do not. As a result, the proportion of SGTF in positive 
