dea7dd34f79a190f4fd92ef689ae5864bc804016
watronfire <snowboardman007@gmail.com>
2021-12-27 13:33:59 -0800

    testing automatic updatedate retrieval

--- src/pages/wastewaterpage.py
+++ src/pages/wastewaterpage.py
@@ -22,8 +22,8 @@ def get_layout( wastewater_data ):
     [GitHub repository](https://github.com/andersen-lab/SARS-CoV-2_WasteWater_San-Diego).
     """
 
-    #commit_date = get_last_commit_date()
-    commit_date = "December 22 @ 1:07 PM PST"
+    commit_date = get_last_commit_date()
+    #commit_date = "December 22 @ 1:07 PM PST"
 
     layout = [
         html.Div(
