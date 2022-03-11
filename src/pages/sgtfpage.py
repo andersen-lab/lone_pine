fc0edc6c9abf29739a98181f9c7cfb83a72d550c
watronfire <snowboardman007@gmail.com>
2022-03-11 10:26:42 -0800

    Changed SGTF text

--- src/pages/sgtfpage.py
+++ src/pages/sgtfpage.py
@@ -5,12 +5,12 @@ def get_layout( sgtf_data, commit_date ):
     markdown = """
     To gain insight into the spread of the Omicron variant in our community, we are working with a large number of 
     partners to track S-gene target failures (SGTFs). SGTFs are a feature of the TaqPath PCR assay that fails to detect
-    the spike gene of certain variants of interest due to a deletion in these viruses' spike gene. Most Omicron 
-    sequences have this deletion while most Delta sequences do not. As a result, the proportion of SGTF in positive 
-    tests can be used to estimate the prevalence of Omicron. The data shown here is collected by our collaboring 
-    partners in San Diego and can be found on our [GitHub repository](https://github.com/andersen-lab/SARS-CoV-2_SGTF_San-Diego). 
-    More information on Omicron and other estimates of its prevalence in San Diego and elsewhere can be found at 
-    [Outbreak.info](https://outbreak.info/).
+    the spike gene of certain variants of interest due to a deletion in these viruses' spike gene. Some Omicron 
+    sequences, particularly BA.1, have this deletion while most Delta sequences and some Omicron sequences, particularly 
+    BA.2, do not. Using this data, we estimate the prevalence of Omicron by fitting a logisitic growth mixture model to 
+    this data. The data shown here is collected by our collaboring partners in San Diego and can be found on our 
+    [GitHub repository](https://github.com/andersen-lab/SARS-CoV-2_SGTF_San-Diego). More information on Omicron and 
+    other estimates of its prevalence in San Diego and elsewhere can be found at [Outbreak.info](https://outbreak.info/).
     """
 
     #commit_date = get_last_commit_date()
