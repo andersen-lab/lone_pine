943cc619323da401ba33e678e6a1435c3d10c2e3
watronfire <snowboardman007@gmail.com>
2022-10-25 10:33:33 -0700

    Corrected zipcodes in sequences.csv and added test

--- test/test_input-files.py
+++ test/test_input-files.py
@@ -4,6 +4,8 @@ import pandas as pd
 VOC_FILE = "resources/voc.txt"
 VOI_FILE = "resources/voi.txt"
 
+SEQ_FILE = "resources/sequences.csv"
+
 def test_voc_file_correct_format():
     with open( VOC_FILE, "r" ) as voc_file:
         assert all( len( line.split( "," ) ) == 2 for line in voc_file ), "VOC file is not the proper format. Two columns are not found in every file."
@@ -12,3 +14,10 @@ def test_voi_file_correct_format():
     with open( VOI_FILE, "r" ) as voi_file:
         assert all( len( line.split( "," ) ) == 2 for line in voi_file ), "VOI file is not the proper format. Two columns are not found in every file."
 
+def test_all_zipcodes_decimal():
+    seqs = pd.read_csv( SEQ_FILE )
+    try:
+        seqs["zipcode"].apply( lambda x: f"{float( x ):.0f}" )
+    except ValueError:
+        # TODO: determine what values are causing the error>
+        assert False, "Some values in zipcode column are not able to be parsed to floats."
