a6c10c4023644532bbfb40523c3204bd336ea2b4
Josh Levy <joshua.levy16@gmail.com>
2023-10-26 15:56:28 -0700

    Update monkeypox.py

--- src/pages/monkeypox.py
+++ src/pages/monkeypox.py
@@ -5,7 +5,7 @@ def get_layout():
     markdown = """
     To monitor the prevalence of Mpox in San Diego, we are measuring virus concentration at the wastewater 
     treatment plants in San Diego. Fragments of Mpox virus DNA are shed in urine and stool and can serve as an 
-    early indicator of the caseload in the community. The data shown here is collected by the Knight Lab at UCSD in 
+    early indicator of the caseload in the community. The data shown here is collected by the EXCITE Lab at UCSD in 
     collaboration with San Diego County, and is available on our [Github repository](https://github.com/andersen-lab/MPX_WasteWater_San-Diego). 
     Scatter points indicate raw data, while solid line represent the same data smoothed with a Savitzky-Golay filter. 
     Hover-over text displays raw values for viral load and smoothed values for reported cases.
